07c84850ce08ca78be5d7878e6c6588e6af1e153
show3567 <ydong011@fiu.edu>
2022-05-03 07:26:22 -0500

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -34,30 +34,3 @@ import { configValidationSchema } from './config/config.schema';
   providers: [AppService],
 })
 export class AppModule {}
-
-/* 
-    Typeorm:
-    npm i @nestjs/typeorm typeorm pg
-
-    Class libs:
-    npm i class-validator class-transformer
-
-    Bcrypt:
-    npm i bcrypt
-
-    JWT:
-    npm i @nestjs/jwt @nestjs/passport passport passport-jwt
-    npm i @types/passport-jwt
-
-    Swagger:
-    npm i @nestjs/swagger swagger-ui-express
-    
-    Config: 
-    npm i @nestjs/config
-    
-    Joi varification: 
-    npm i @hapi/joi
-    
-    Hold JWT secure: 
-    https://passwordsgenerator.net/
-*/
--- src/main.ts
+++ src/main.ts
@@ -23,3 +23,30 @@ async function bootstrap() {
   logger.log(`Application listening on port ${port}`);
 }
 bootstrap();
+
+/* 
+    Typeorm:
+    npm i @nestjs/typeorm typeorm pg
+
+    Class libs:
+    npm i class-validator class-transformer
+
+    Bcrypt:
+    npm i bcrypt
+
+    JWT:
+    npm i @nestjs/jwt @nestjs/passport passport passport-jwt
+    npm i @types/passport-jwt
+
+    Swagger:
+    npm i @nestjs/swagger swagger-ui-express
+    
+    Config: 
+    npm i @nestjs/config
+    
+    Joi varification: 
+    npm i @hapi/joi
+    
+    Strong JWT secret: 
+    https://passwordsgenerator.net/
+*/
