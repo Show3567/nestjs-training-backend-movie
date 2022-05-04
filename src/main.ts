f3ee2f0bfdbcb59f4347460e95ffcbcebd27a730
show3567 <ydong011@fiu.edu>
2022-05-04 08:00:09 -0500

    update

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,17 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
+import * as CookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const logger = new Logger();
 
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    credentials: true,
+    origin: 'http://localhost:4200',
+  });
+  app.use(CookieParser());
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -49,4 +55,7 @@ bootstrap();
     
     Strong JWT secret: 
     https://passwordsgenerator.net/
+
+    Cookie-parser
+    npm i cookie-parser
 */
