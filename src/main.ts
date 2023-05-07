2fc00f601ec65a1b9131a79662972283c32ff4c8
Yue Dong <ydong011@fiu.edu>
2023-05-07 13:54:09 -0500

    update

--- src/main.ts
+++ src/main.ts
@@ -91,15 +91,3 @@ bootstrap();
     $ npm i --save @nestjs/microservices
 
 */
-
-
-// new Promise((resolve, reject) => {
-//   console.log('hello')
-// });
-
-// of('hello').pipe(
-//   map(date => {
-//     console.log('hello')
-//     return data
-//   })
-// );
