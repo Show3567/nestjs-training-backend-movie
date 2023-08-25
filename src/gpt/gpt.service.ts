96dc4f63fc0b3397c907d08c7599d46a9842e69c
Yue Dong <ydong011@fiu.edu>
2023-08-25 16:35:05 -0500

    update

--- src/gpt/gpt.service.ts
+++ src/gpt/gpt.service.ts
@@ -1,18 +1,29 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { RequestOptions } from 'https';
+
+import OpenAI from 'openai';
+const { Configuration, OpenAIApi } = require('openai');
 
 @Injectable()
 export class GptService {
-  config: RequestOptions = {
-    headers: {
-      Authorization: `Bearer ${this.configService.get('OPENAI_API_KEY')}`,
-    },
-  };
+  readonly config = new Configuration({
+    apiKey: this.configService.get('OPENAI_API_KEY'),
+    organization: this.configService.get('OPENAI_ORG_ID'),
+  });
+  openai = new OpenAIApi(this.config);
 
-  constructor(private readonly configService: ConfigService) {
-    console.log(this.config.headers);
+  constructor(private readonly configService: ConfigService) {}
+
+  async sendRequest(msg: string) {
+    const res = await this.openai.CreateChatCompletion({
+      model: 'gpt-3.5-turbo',
+      messages: [
+        {
+          role: 'user',
+          content: 'hello',
+        },
+      ],
+    });
+    console.log(res);
   }
-
-  sendQuestion() {}
 }
