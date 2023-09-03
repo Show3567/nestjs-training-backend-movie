8c7ba39ca099c5fae846aa7042f3fae876917682
Yue Dong <ydong011@fiu.edu>
2023-09-02 21:02:55 -0500

    update

--- src/gpt/gpt.module.ts
+++ src/gpt/gpt.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { GptController } from './gpt.controller';
 import { GptService } from './gpt.service';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [ConfigModule],
+  imports: [ConfigModule, HttpModule],
   controllers: [GptController],
   providers: [GptService],
 })
--- src/gpt/gpt.service.ts
+++ src/gpt/gpt.service.ts
@@ -1,29 +1,19 @@
+import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-
 import OpenAI from 'openai';
-const { Configuration, OpenAIApi } = require('openai');
 
 @Injectable()
 export class GptService {
-  readonly config = new Configuration({
+  private readonly openAi = new OpenAI({
     apiKey: this.configService.get('OPENAI_API_KEY'),
     organization: this.configService.get('OPENAI_ORG_ID'),
   });
-  openai = new OpenAIApi(this.config);
 
-  constructor(private readonly configService: ConfigService) {}
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly http: HttpService,
+  ) {}
 
-  async sendRequest(msg: string) {
-    const res = await this.openai.CreateChatCompletion({
-      model: 'gpt-3.5-turbo',
-      messages: [
-        {
-          role: 'user',
-          content: 'hello',
-        },
-      ],
-    });
-    console.log(res);
-  }
+  async getModelAnswer(question: string) {}
 }
